c00394bc44e92c4d3cdb91b4c7958d7210b53bae
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2020-12-01 17:36:54 +0100

    spring security intro env setup down

---
+++ spring-security-intro/src/main/java/com/foureverhh/security/springmvc/init/SpringApplicationInitializer.java
@@ -0,0 +1,22 @@
+package com.foureverhh.security.springmvc.init;
+
+import com.foureverhh.security.springmvc.config.ApplicationConfig;
+import com.foureverhh.security.springmvc.config.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{ApplicationConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ spring-security-intro/src/main/webapp/WEB-INF/view/login.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: foureverhh
+  Date: 2020-12-01
+  Time: 17:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>login</title>
+</head>
+<body>
+    <h3>Log in</h3>
+    <form action="login" method="post">
+        Username: <input type="text" name="username"> <br>
+        password: <input type="password" name="password"> <br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
