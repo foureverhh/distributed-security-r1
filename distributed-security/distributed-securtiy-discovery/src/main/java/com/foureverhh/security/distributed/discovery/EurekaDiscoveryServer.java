70ca540959e9b7a612cf78ef6008e04f64c60b64
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2021-01-02 17:00:41 +0100

    setup eureka server

---
+++ distributed-security/distributed-securtiy-discovery/src/main/java/com/foureverhh/security/distributed/discovery/EurekaDiscoveryServer.java
@@ -0,0 +1,13 @@
+package com.foureverhh.security.distributed.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaDiscoveryServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDiscoveryServer.class,args);
+    }
+}
