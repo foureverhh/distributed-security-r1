369f19881f24336e1f8d6915d927d08cfcdcc573
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2021-01-03 16:38:16 +0100

    gateway set up

---
+++ distributed-security/distributed-security-gateway/src/main/java/com/foureverhh/security/gateway/config/ResourceServerConfig.java
@@ -0,0 +1,80 @@
+package com.foureverhh.security.gateway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@Configuration
+public class ResourceServerConfig {
+    public static final String RESOURCE_ID="res1";
+
+    //uaa resource
+    @Configuration
+    @EnableResourceServer
+    public class UAAServerConfig extends ResourceServerConfigurerAdapter{
+        @Autowired
+        private TokenStore tokenStore;
+
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+            resources.tokenStore(tokenStore).stateless(true);
+        }
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/uaa/**").permitAll();
+        }
+    }
+
+    //order resource
+    @Configuration
+    @EnableResourceServer
+    public class OrderServerConfig extends ResourceServerConfigurerAdapter{
+        @Autowired
+        private TokenStore tokenStore;
+
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+            resources.tokenStore(tokenStore).stateless(true);
+        }
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/order/**").access("#oauth2.hasScope('ROLE_API')");
+        }
+    }
+
+    /*
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").access("#oauth2.hasScope('all')")
+                .and().csrf().disable()
+                //Based on token,session do not need to store state
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); //Based on token,session do not need to store state
+    }
+
+     */
+
+    /*
+    //remote authenticate token
+    @Bean("resourceServerTokenServices")
+    public ResourceServerTokenServices tokenServices(){
+        RemoteTokenServices services = new RemoteTokenServices();
+        services.setCheckTokenEndpointUrl("http://localhost:53020/uaa/oauth/check_token");
+        services.setClientId("c1");
+        services.setClientSecret("secret");
+        return services;
+    }
+     */
+
+}
